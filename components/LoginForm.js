8aba35644961f9496b737b4cc1ffb0882d94799d
Artem Sychov <artem.suchov@gmail.com>
2019-08-01 11:50:28 +0300

    create login form, handle post request on server

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,3 +1,5 @@
+import { loginUser } from "../lib/auth";
+
 class LoginForm extends React.Component {
   state = {
     email: "",
@@ -10,8 +12,9 @@ class LoginForm extends React.Component {
     });
   };
   handleSubmit = event => {
+    const { email, password } = this.state;
     event.preventDefault();
-    console.log(this.state);
+    loginUser(email, password);
   };
 
   render() {
