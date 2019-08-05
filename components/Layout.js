4d5d2e64d763353dd59635d94fe73e46761276cc
Artem Sychov <artem.suchov@gmail.com>
2019-08-05 12:55:37 +0300

    fix small errors

--- components/Layout.js
+++ components/Layout.js
@@ -6,19 +6,19 @@ const Layout = ({ title, children }) => {
       <nav className="navbar">
         <span>
           Welcome, <strong>Guest</strong>
-          <div>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-            <Link href="/profile">
-              <a>Profile</a>
-            </Link>
-            <button>Logout</button>
-            <Link href="/login">
-              <a>Login</a>
-            </Link>
-          </div>
         </span>
+        <div>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/profile">
+            <a>Profile</a>
+          </Link>
+          <button>Logout</button>{" "}
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+        </div>
       </nav>
 
       <h1>{title}</h1>
